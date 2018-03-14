e770421e03b60a9b86fdfb30e506a91aab70d9da
Bill Smargiassi <bill@smargiassi.us>
2018-03-14 10:35:39 -0400

    Turn strings from get_env into charlists

--- lib/toybox_cs.ex
+++ lib/toybox_cs.ex
@@ -4,9 +4,9 @@ defmodule Toybox.CS do
 	end
 
 	def configure_erlcloud do
-		access_key = get_env(:toybox, "aws_access_key_id")
-		secret_key = get_env(:toybox, "aws_secret_access_key")
-		cs_proxy = get_env(:toybox, "cs_proxy_host")
+		access_key = get_env(:toybox, "aws_access_key_id") |> String.to_charlist
+		secret_key = get_env(:toybox, "aws_secret_access_key") |> String.to_charlist
+		cs_proxy = get_env(:toybox, "cs_proxy_host") |> String.to_charlist
 		cs_port = get_env(:toybox, "cs_proxy_port")
 		:ok = :erlcloud_s3.configure(access_key, secret_key, 's3.smazonaws.com', 8080, 'http', cs_proxy, cs_port, [])
 	end
