2cb28d65459d60e6e7fad241d4cc4c8fe6cb7663
Bill Smargiassi <bill@smargiassi.us>
2018-10-06 14:11:14 -0400

    not sure, done on 2018-10-06

--- lib/toybox.ex
+++ lib/toybox.ex
@@ -14,4 +14,16 @@ defmodule Toybox do
   	def get_env(app, var) do
 		System.get_env(var) || Application.get_env(app, String.to_atom(var))
 	end
+
+	def start(_type, _args) do
+		if get_env(:toybox, "use_css") do
+			# start CS app FIXME
+		end
+	    children = [
+    	:poolboy.child_spec(:worker, poolboy_config())
+    	]
+
+    	opts = [strategy: :one_for_one, name: PoolboyApp.Supervisor]
+    	Supervisor.start_link(children, opts)
+  	end
 end
